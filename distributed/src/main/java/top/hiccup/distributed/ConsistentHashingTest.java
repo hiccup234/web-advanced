2acce28fd342c1428799b174831d210326480644
wenhy <haiyang.wen@56qq.com>
2019-01-21 20:46:46 +0800

    cap and so on

---
+++ distributed/src/main/java/top/hiccup/distributed/ConsistentHashingTest.java
@@ -0,0 +1,13 @@
+package top.hiccup.distributed;
+
+/**
+ * 【一致性Hash算法】
+ * 一致性哈希算法在1997年由麻省理工学院的Karger等人在解决分布式Cache中提出的，设计目标是为了解决因特网中的热点(Hot spot)问题，初衷和CARP十分类似。
+ *
+ *
+ *
+ * @author wenhy
+ * @date 2019/1/21
+ */
+public class ConsistentHashingTest {
+}
