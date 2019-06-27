d87c768cd96f4f67c308aa623fb85b4df0f011d7
wenhy <haiyang.wen@56qq.com>
2019-06-27 21:39:09 +0800

    组成原理

---
+++ advanced/计算机/1_计算机组成原理/3_处理器/分支预测.java
@@ -0,0 +1,28 @@
+/**
+ * 冒险与分支预测：今天下雨了，明天还会下雨吗？
+ */
+public class BranchPrediction {
+    public static void main(String args[]) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j <1000; j ++) {
+                for (int k = 0; k < 10000; k++) {
+                }
+            }
+        }
+        long end = System.currentTimeMillis();
+        // 5ms
+        System.out.println("Time spent in first loop is " + (end - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j <1000; j ++) {
+                for (int k = 0; k < 100; k++) {
+                }
+            }
+        }
+        // 15ms
+        end = System.currentTimeMillis();
+        System.out.println("Time spent in second loop is " + (end - start) + "ms");
+    }
+}
