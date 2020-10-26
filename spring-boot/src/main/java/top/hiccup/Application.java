fa2a60505ac5757437899575ab47637043f32062
hiccup234 <wenhaiyang3@jd.com>
2020-10-26 22:43:18 +0800

    im

--- spring-boot/src/main/java/top/hiccup/Application.java
+++ spring-boot/src/main/java/top/hiccup/Application.java
@@ -19,5 +19,6 @@ public class Application {
      */
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ctx.registerShutdownHook();
     }
 }
